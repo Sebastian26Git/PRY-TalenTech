53fd59aa20274dac60acb5367719266d869996fc
Sebastian Baron <sebastianbaron26@gmail.com>
2024-10-16 19:59:50 -0500

    incluye despliegue rds

--- modules/VPC/output.tf
+++ modules/VPC/output.tf
@@ -3,12 +3,13 @@ output "vpc_id" {
 }
 
 output "public_subnets_id" {
-  value = [aws_subnet.public_subnet.*.id]
+  value = ["${aws_subnet.public_subnet.*.id}"]
 }
 
 output "private_subnets_id" {
-  value = [aws_subnet.private_subnet.*.id]
+  value = ["${aws_subnet.private_subnet.*.id}"]
 }
+
 
 output "default_sg_id" {
   value = "aws_security_group.default.id"
